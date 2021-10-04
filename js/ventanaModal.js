d8398b1744339c9982a26ce36c6e33a3e99b1b4c
lisandro cordoba <lisandrocba_7@hotmail.com>
2021-10-04 11:44:41 -0300

    prueba formulario

---
+++ js/ventanaModal.js
@@ -0,0 +1,29 @@
+let cerrar = document.querySelectorAll(".cerrar")[0];
+let abrir = document.querySelectorAll(".Trabajo")[0];
+let ventana = document.querySelectorAll(".ventana")[0];
+let contenedorVentana = document.querySelectorAll(".contenedorVentana")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    contenedorVentana.style.opacity = "1";
+    contenedorVentana.style.visibility = "visible";
+    ventana.classList.toggle("ventanaCerrar")
+});
+
+cerrar.addEventListener("click", function(){
+    ventana.classList.toggle("ventanaCerrar");
+    setTimeout(function(){
+        contenedorVentana.style.opacity = "0";
+        contenedorVentana.style.visibility = "hidden";
+    },500)
+});
+
+window.addEventListener("click", function(e){
+    if(e.target == contenedorVentana){
+        ventana.classList.toggle("ventanaCerrar");
+        setTimeout(function(){
+            contenedorVentana.style.opacity = "0";
+            contenedorVentana.style.visibility = "hidden";
+        },500)
+    }
+})
---
+++ php/envias.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$linkedin = $_POST['linkedin'];
+$cv = $_POST['cv'];
+
+$destinatario = "lisandrocba7@gmail.com";
+$asunto = "Envio de pagina web who consultora"
+
+$carta = "De: $nombre \n";
+$carta .= "mail: $mail \n";
+$carta .= "linkedin $linkedin \n";
+$carta.= "cv: $cv";
+
+mail($destinatario, $asunto, $carta);
+header('location:trabajo.html');
+
+?>
