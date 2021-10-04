585016ee9c3649f4a5d7581893867cbe75e8f92c
lisandro cordoba <lisandrocba_7@hotmail.com>
2021-10-04 11:53:58 -0300

    correccion form

--- php/envias.php
+++ php/envias.php
@@ -6,12 +6,12 @@ $linkedin = $_POST['linkedin'];
 $cv = $_POST['cv'];
 
 $destinatario = "lisandrocba7@gmail.com";
-$asunto = "Envio de pagina web who consultora"
+$asunto = "Envio de pagina web who consultora";
 
 $carta = "De: $nombre \n";
 $carta .= "mail: $mail \n";
 $carta .= "linkedin $linkedin \n";
-$carta.= "cv: $cv";
+//$carta.= "cv: $cv";
 
 mail($destinatario, $asunto, $carta);
 header('location:trabajo.html');
